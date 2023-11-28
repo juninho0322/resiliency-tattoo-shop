39c6f16335d6b8c5857819ea995232d35ab9e027
Luis <juninho0322@gmail.com>
2023-11-28 02:55:04 +0000

    menu-home non responsive

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// alert(`testing`);
+
+
+
+var lightbox = new PhotoSwipeLightbox({
+    gallery: '.test-gallery',
+    children: 'a',
+    // dynamic import is not supported in UMD version
+    pswpModule: PhotoSwipe
+});
+
+lightbox.init();
+
+document.querySelector("#button-book").addEventListener("click", function () {
+    alert(`test`);
+    console.log(`test`);
+});
